d854fec7f908359084a2d61b4bda2def05fdef60
Dardan Rrafshi <dardan.rrafshi@gmail.com>
2023-06-12 21:35:26 +0200

    Implemented dynamic generation of service tiles

--- webapp/controller/Main.controller.ts
+++ webapp/controller/Main.controller.ts
@@ -1,5 +1,9 @@
-import MessageBox from "sap/m/MessageBox";
+import ImageContent from "sap/m/ImageContent";
 import BaseController from "./BaseController";
+import Event from "sap/ui/base/Event";
+import Context from "sap/ui/model/Context";
+import TileContent from "sap/m/TileContent";
+import GenericTile from "sap/m/GenericTile";
 
 
 /**
@@ -7,8 +11,33 @@ import BaseController from "./BaseController";
  */
 export default class Main extends BaseController
 {
-	public sayHello()
+	public createServiceTiles(id: string, context: Context)
 	{
-		MessageBox.show("Hello World!");
+		const serviceID = context.getProperty("id"),
+			title = context.getProperty("title"),
+			subtitle = context.getProperty("subtitle"),
+			icon = context.getProperty("icon");
+	
+		const imageContent = new ImageContent({
+			src: icon
+		});
+	
+		const tileContent = new TileContent();
+		tileContent.setContent(imageContent);
+		
+		const tile = new GenericTile(serviceID, {
+			header: title,
+			subheader: subtitle
+		});
+		tile.addTileContent(tileContent);
+		tile.attachPress(this.onServiceSelect, this);
+		
+		return tile;
+	}
+
+	public onServiceSelect(event: Event)
+	{
+		const serviceID = event.getParameter("id");
+		this.navigateTo(serviceID);
 	}
 }
